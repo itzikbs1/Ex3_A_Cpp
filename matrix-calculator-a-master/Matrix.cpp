b00489403309f0bda73dc2c3abdeb7cb0f0a2df3
itzik <itzik3877@gmail.com>
2022-04-01 15:16:14 +0300

    h

---
+++ matrix-calculator-a-master/Matrix.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+// #include <vector>
+// #include <vecintrin.h>
+#include "Matrix.hpp"
+#include <stdexcept>
+
+using namespace std;
+using namespace zich;
+
+        Matrix::Matrix(){
+            
+        }
+        Matrix::Matrix(vector<double> mat, int row, int col){
+
+        }
+        Matrix Matrix::operator+(Matrix a){
+                return Matrix();
+            }
+
+        Matrix Matrix::operator+(double scalr){
+            return Matrix();
+        }
+        Matrix Matrix::operator+(){
+            return Matrix();
+        }
+
+        Matrix Matrix::operator+=(Matrix a){
+            return Matrix();
+        }
+        Matrix Matrix::operator+=(double scalr){
+            return Matrix();
+        }
+        Matrix Matrix::operator-(Matrix a){
+            return Matrix();
+        }
+
+       Matrix Matrix::operator-(double scalr){
+            return Matrix();
+        }
+        Matrix Matrix::operator-(){
+            return Matrix();
+        }
+        Matrix Matrix::operator-=(Matrix a){
+            return Matrix();
+        }
+        Matrix Matrix::operator-=(double scalr){
+            return Matrix();
+        }
+        Matrix Matrix::operator>(Matrix a){
+            return Matrix();
+        }
+        Matrix Matrix::operator>=(Matrix a){
+            return Matrix();
+        }
+        Matrix Matrix::operator<(Matrix a){
+            return Matrix();
+        }
+        Matrix Matrix::operator<=(Matrix a){
+            return Matrix();
+        }
+        Matrix Matrix::operator==(Matrix a){
+            return Matrix(); 
+        }
+        Matrix Matrix::operator!=(Matrix a){
+            return Matrix();
+        }
+        Matrix Matrix::operator*(double scalr){
+            return Matrix();
+        }
+        Matrix Matrix::operator*(Matrix a){
+            return Matrix();
+        }
+        Matrix operator*(double scalr, Matrix mat){
+            return Matrix();
+        }
+        Matrix Matrix::operator*=(Matrix a){
+            return a;
+        }
+        Matrix Matrix::operator*=(double scalr){
+            return Matrix();
+        }
+        ostream& operator<<(ostream& os, Matrix &m){
+            return os;
+        }
+        //hello
+        // Matrix::istream& operator>>(istream& in, Matrix m){
+            // return in;
+        // }
---
+++ matrix-calculator-a-master/Matrix.hpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+// #include <vecintrin.h>
+
+using namespace std;
+namespace zich{
+
+
+
+class Matrix{
+
+        private:
+        vector<double> mat;
+        int row;
+        int col;
+
+        // Matrix(){
+        //     this->mat = {};
+        //     this->row = 0;
+        //     this->col = 0;
+        // }
+        // Matrix(std::vector<double> mat={}, int row=0, int col=0){Matrix::mat = mat, Matrix::row = row, Matrix::col = col;}
+
+            // this->mat = mat;
+            // this->row = row;
+            // this->col = col;
+        // }
+
+        // Matrix(vector<double> mat={}, int row=0, int col=0){
+        //     this->mat = mat;
+        //     this->row = row;
+        //     this->col = col;
+        // }
+        // Matrix(Matrix m){
+        //     this->mat = m.mat;
+        //     this->row = m.row;
+        //     this->col = m.col;
+        // }
+        public:
+        Matrix();
+        Matrix(vector<double> mat, int row, int col);
+        Matrix operator+(Matrix a);
+        Matrix operator+(double scalr);
+        Matrix operator+();
+        Matrix operator+=(Matrix a);
+        Matrix operator+=(double scalr);
+        Matrix operator-(Matrix a);
+        Matrix operator-(double scalr);
+        Matrix operator-();
+        Matrix operator-=(Matrix a);
+        Matrix operator-=(double scalr);
+        Matrix operator>(Matrix a);
+        Matrix operator>=(Matrix a);
+        Matrix operator<(Matrix a);
+        Matrix operator<=(Matrix a);
+        Matrix operator==(Matrix a);
+        Matrix operator!=(Matrix a);
+        friend ostream& operator<<(ostream& os, Matrix &m);
+        // friend istream& operator>>(istream& os, Matrix m);
+        Matrix operator*(double scalr);
+        Matrix operator*(Matrix mat);
+        // friend Matrix operator*(Matrix mat, double scalr);
+        friend Matrix operator*(double scalr, Matrix mat);
+        Matrix operator*=(Matrix mat);
+        Matrix operator*=(double scalr);
+        // void add(Matrix m1, Matrix m2);
+        // void add(Matrix m1, double scalr);
+
+        // void (Matrix m1, Matrix m2);
+
+
+    };
+}
